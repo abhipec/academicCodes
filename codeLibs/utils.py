be8c5521aa8b996fb7d927a505ac2ceb9f1731e8
abhishek <abhishek.abhishek@iitg.ernet.in>
2015-01-30 19:32:15 +0530

    added function to compute Manhattan distance on square matrix changed representation of blank block from 'X' to '0'

--- codeLibs/utils.py
+++ codeLibs/utils.py
@@ -4,9 +4,34 @@ def calculateInversions(data):
 	inversion = 0
 	for idx, val in enumerate(data):
 		for val2 in data[idx+1:]:
-			if val2 != 'X' and str(val2) < str(val):
+			if val2 != '0' and str(val2) < str(val):
 				inversion += 1
 	return inversion
+
+def computeSingleMoveManhattanDistance(start, end, matrixSize):
+	'''	It returns number of moves required to reach end state from start state in a matrix environment'''
+	if start > end:
+		tmp = start
+		start = end
+		end = tmp
+	distance = 0
+	while ( end - start ) >= matrixSize:
+		end -= matrixSize
+		distance += 1
+	if ( end - start ) == 1 and ( start == 2 or start == 5 ):
+		distance += 3
+	else:
+		distance += end - start
+	return distance
+
+def calculateManhattanDistance(state, goalState, matrixSize):
+	'''	It will calculate Manhattan distance of a state w.r.t goal state
+		assuming a square matrix of matrixSize'''
+	distance = 0
+	for i in range(len(state)):
+		if state[i] != '0':
+			distance += computeSingleMoveManhattanDistance(i, goalState.find(state[i]), matrixSize)
+	return distance
 
 def swap(data, idx1, idx2):
 	'''	returns a string with content at location idx1 and idx2 swapped
@@ -16,3 +41,4 @@ def swap(data, idx1, idx2):
 		idx1 = idx2
 		idx2 = tmp
 	return data[0:idx1] + data[idx2] + data[idx1+1:idx2] + data[idx1] + data[idx2+1:]
+
