dfb76e1c82975770a1c671e431edf5f3c35eba6b
abhishek <abhishek.abhishek@iitg.ernet.in>
2015-02-19 19:36:38 +0530

    1.11 solved

---
+++ prolog99/1.11.pl
@@ -0,0 +1,13 @@
+pack(X,Y) :- pack(X,Y,[]).
+pack([],[]).
+pack([X],[R],A) :- append(A,[X],R).
+pack([X,X|T],R,A) :- append(A,[X],Anew),pack([X|T],R,Anew).
+pack([X,Y|T],[Anew|R],A) :- X \= Y, append(A,[X],Anew), pack([Y|T],R,[]).
+
+encode(X,Y) :- pack(X,Ynew), combine(Ynew,Y). %first implement 1.09 then combine its output
+
+combine([H|T],[A|Y]) :- compute(H,A), combine(T,Y). 
+combine([],[]).
+
+compute([H|T],A) :- length([H|T],C), (C \= 1 -> A = [C,H]; A = H).
+
